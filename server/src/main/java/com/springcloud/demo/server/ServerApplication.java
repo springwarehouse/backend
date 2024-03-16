0dc00614cfd8b9257bb5ed8fb341e286dae44ab6
zhenzhou yang <zhenzhou.yang@outlook.com>
2024-03-16 19:18:21 +0800

    新增项目

---
+++ server/src/main/java/com/springcloud/demo/server/ServerApplication.java
@@ -0,0 +1,12 @@
+package com.springcloud.demo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
+
+@SpringBootApplication(scanBasePackages = {"com.springcloud.demo.server"}, exclude = {GsonAutoConfiguration.class})
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
