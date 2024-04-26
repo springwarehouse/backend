f0fad8c776c7e5d51d35dd0fdce5b26cba7aee07
zhenzhou yang <zhenzhou.yang@outlook.com>
2024-04-26 21:44:35 +0800

    处理websocket消息

--- server/src/main/java/com/springcloud/demo/server/core/exception/WSException.java
+++ server/src/main/java/com/springcloud/demo/server/core/exception/WSException.java
@@ -1,7 +1,15 @@
 package com.springcloud.demo.server.core.exception;
 
+/**
+ * WebSocket异常
+ */
 public class WSException extends Exception {
 
+    /**
+     * 构造函数
+     *
+     * @param message 异常信息
+     */
     public WSException(String message) {
         super(message);
     }
